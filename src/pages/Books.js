cee67dc048b77ce0baf10ffa2e178c77adfddc33
Debabrata Mandal <bapidebabrata@gmail.com>
2022-07-09 01:59:34 +0530

    Add Books page component

---
+++ src/pages/Books.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddBook from '../components/AddBook';
+import BookList from '../components/BookList';
+
+const Books = () => (
+  <main>
+    <BookList />
+    <AddBook />
+  </main>
+);
+
+export default Books;
