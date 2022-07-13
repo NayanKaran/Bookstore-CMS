ffc05638d55f0a3d96f91e50056a753122249950
Debabrata Mandal <bapidebabrata@gmail.com>
2022-07-13 22:54:01 +0530

    show booklist from Redux store

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,30 +1,9 @@
+import { useSelector } from 'react-redux';
 import React from 'react';
 import Book from './Book';
 
 const BookList = () => {
-  const books = [
-    {
-      id: '0',
-      name: 'The Alchemist',
-      genre: 'Fiction',
-      author: 'Paulo Coelho',
-      progress: {
-        percentage: 0,
-        currentChapter: 'Introduction',
-      },
-    },
-    {
-      id: '1',
-      name: 'Harry Potter and the Sorcerer\'s Stone',
-      genre: 'Fiction',
-      author: 'J.K. Rowling',
-      progress: {
-        percentage: 0,
-        currentChapter: 'Introduction',
-      },
-    },
-  ];
-
+  const books = useSelector((state) => state.entities.books);
   const listItems = books.map((book) => (
     <li key={book.id}>
       <Book details={book} />
