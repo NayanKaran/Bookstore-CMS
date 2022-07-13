da7017fad44a85ef5bb75bd1fa3c46e20be7bb0a
Debabrata Mandal <bapidebabrata@gmail.com>
2022-07-13 22:51:25 +0530

    add book details to redux store using useDispatch

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,14 +1,31 @@
+import { useDispatch } from 'react-redux';
 import React from 'react';
+import { bookAdded } from '../store/books';
 
-const AddBook = () => (
-  <div className="add-book">
-    <h3>Add New Book</h3>
-    <form>
-      <input type="text" placeholder="Book title" name="title" required />
-      <input type="text" placeholder="Book Author" name="author" required />
-      <button type="submit">Add Book</button>
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="add-book">
+      <h3>Add New Book</h3>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(
+            bookAdded({
+              name: e.target.elements.title.value,
+              author: e.target.elements.author.value,
+            }),
+          );
+          e.target.reset();
+        }}
+      >
+        <input type="text" placeholder="Book title" name="title" required />
+        <input type="text" placeholder="Book Author" name="author" required />
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
